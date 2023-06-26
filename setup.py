d1728174b9c4446212bedee55274ff831fe6fcbf
Eli Corpron <ecorpron@gmail.com>
2023-06-26 15:14:35 -0700

    attempting to fix install problems

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='SteamApiAccessor',
     version='1.0',
     description='A module to access the public Steam API with some helpful methods to manipulate the returned request',
     author='Eli Corpron',
-    packages=['SteamApiAccessor'],  # same as name
+    packages=find_packages(),  # same as name
     install_requires=['requests'],  # external packages as dependencies
 )
