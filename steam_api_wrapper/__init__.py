58763db20dbd3977584c44d332103a3b48636aee
Eli Corpron <ecorpron@gmail.com>
2023-06-26 16:57:31 -0700

    restructured package so that it could be easier to be installed

---
+++ steam_api_wrapper/__init__.py
@@ -0,0 +1,4 @@
+from .classes import Account, Game, GameCollection
+from .exceptions import InvalidResponseException
+from .games_manager import get_random_game, get_games_dict, get_game_count
+from .steam_api_controller import SteamAPIController
--- steam_api_wrapper/classes.py
+++ steam_api_wrapper/classes.py
@@ -19,6 +19,7 @@ class Game:
         output = output + "Total Playtime: {}\n".format(self.total_playtime)
         return output
 
+
 class GameCollection:
 
     def __init__(self, game_count=0, game_dict=None):
